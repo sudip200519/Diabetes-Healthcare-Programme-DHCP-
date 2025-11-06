9ff823dffb431355ba4fbf499e48c1fa9b200041
sudip200519 <163457230+sudip200519@users.noreply.github.com>
2025-11-06 12:16:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,7 @@ st.set_page_config(
     layout = 'wide',
     initial_sidebar_state = 'auto'
 )
-#extra
-col1, col2 = st.columns([8, 2])
-with col2:
-    theme = st.selectbox("Theme", ["Light", "Dark"])
 
-if theme == "Light":
-    bg_color = "#ffffff"
-    text_color = "#000000"
-else:
-    bg_color = "#0e1117"
-    text_color = "#fafafa"
-
-st.markdown(
-    f"""
-    <style>
-    body {{
-        background-color: {bg_color};
-        color: {text_color};
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
- # break
 
 Tabs = {
     "Home":home,
